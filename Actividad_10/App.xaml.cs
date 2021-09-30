171a76ae66f904af058fa2bb674872c7926d34a5
Alex M <cmaldonado0@ucol.mx>
2021-09-30 17:00:50 -0500

    Add files via upload

---
+++ Actividad_10/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Actividad_10.Modelo;
+using Actividad_10.Vistas;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+
+namespace Actividad_10
+{
+    public partial class App : Application
+    {
+        public static List<Persona> Personas { get; set; }
+        public static bool tapped { get; set; }
+        public static int index { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            Personas = new List<Persona>();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Actividad_10/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Actividad_10.Vistas;
+using Actividad_10.Modelo;
+
+namespace Actividad_10
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            App.tapped = false;
+            btnModificar.IsEnabled = false;
+            btnEliminar.IsEnabled = false;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            lstPersonas.ItemsSource = null;
+            lstPersonas.ItemsSource = App.Personas;
+        }
+        private void btnNuevo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NuevoPage());
+        }
+        private void btnModificar_Clicked(object sender, EventArgs e)
+        {
+            if (App.tapped)
+            {
+                App.Personas[App.index].Nombre = txtNombre.Text;
+                App.Personas[App.index].Correo = txtCorreo.Text;
+                App.Personas[App.index].Telefono = txtTelefono.Text;
+                lstPersonas.ItemsSource = null;
+                lstPersonas.ItemsSource = App.Personas;
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            if (App.tapped)
+            {
+                App.Personas.RemoveAt(App.index);
+                lstPersonas.ItemsSource = null;
+                lstPersonas.ItemsSource = App.Personas;
+                App.tapped = false;
+                btnModificar.IsEnabled = false;
+                btnEliminar.IsEnabled = false;
+                txtNombre.Text = "";
+                txtCorreo.Text = "";
+                txtTelefono.Text = "";
+            }
+        }
+
+        private void lstPersonas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var myListView = (ListView)sender;
+            var myItem = myListView.SelectedItem;
+            App.index = App.Personas.IndexOf(myItem);
+            txtNombre.Text = App.Personas[App.index].Nombre;
+            txtCorreo.Text = App.Personas[App.index].Correo;
+            txtTelefono.Text = App.Personas[App.index].Telefono;
+            App.tapped = true;
+            btnModificar.IsEnabled = true;
+            btnEliminar.IsEnabled = true;
+        }
+
+
+    }
+}
